71f2b21fa55e0fe75000c9946af17b880ce4e69c
George Hertz <hertzg@users.noreply.github.com>
2023-04-20 23:43:17 +0200

    feat!: convert to esm (#104)

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,11 +1,7 @@
-export type g_date_is_leap_year = (year: number) => boolean;
-export type g_julian_days_to_date = (julian_days: number) => Date;
-export type g_date_to_julian_days = (date: Date) => number;
+export type isLeapYear = (year: number) => boolean;
+export type julianDaysToDate = (julian_days: number) => Date;
+export type dateToJulianDays = (date: Date) => number;
 
-export const g_date_is_leap_year: g_date_is_leap_year
-export const gIsLeapYear: g_date_is_leap_year
-export const g_julian_days_to_date: g_julian_days_to_date
-export const gJulianDays2Date: g_julian_days_to_date
-export const g_date_to_julian_days: g_date_to_julian_days
-export const gDate2JulianDays: g_date_to_julian_days
-
+export const isLeapYear: isLeapYear;
+export const julianDaysToDate: julianDaysToDate;
+export const dateToJulianDays: dateToJulianDays;
